4845097862ab16c7710078279828e67b3e1907c5
Jensen <sims.jensen08@gmail.com>
2026-08-18 15:30:51 +0930

    Add VehicleController & InputReader; update scene
    Add VehicleController MonoBehaviour (Assets/Scripts/VehicleController.cs) with axle handling, steering/motor/brake logic and a Vector3 extension. Add InputReader ScriptableObject (Assets/Scripts/ScriptableObjects/InputReader.asset) and folder meta. Update SampleScene.unity: add/configure a Vehicle GameObject (Rigidbody, BoxCollider, WheelColliders, VehicleController component), adjust prefab/mesh references and version indices, and tidy stripped/serialized mesh entries. Add corresponding .meta files for new assets. These changes add vehicle runtime/input infrastructure and update scene prefab wiring to use the new assets.

---
+++ DriftWash/Assets/Scripts/VehicleController.cs
@@ -0,0 +1,154 @@
+using DriftWash;
+using UnityEngine;
+
+namespace DriftWash
+{
+    [System.Serializable]
+    public class AxleInfo
+    {
+        public WheelCollider leftWheel;
+        public WheelCollider rightWheel;
+        public bool motor;
+        public bool steering;
+        public WheelFrictionCurve originalForwardFriction;
+        public WheelFrictionCurve originalSidewaysFriction;
+    }
+
+    public class VehicleController : MonoBehaviour
+    {
+        [Header("Axle Information")]
+        [SerializeField] AxleInfo[] axleInfos;
+
+        [Header("Motor Attributes")]
+        [SerializeField] float maxMotorTorque = 3000f;
+        [SerializeField] float maxSpeed;
+
+        [Header("Steering Attributes")]
+        [SerializeField] float maxSteeringAngle = 30f;
+
+        [Header("Braking and Drifting Attributes")]
+        [SerializeField] float brakeTorque = 10000f;
+
+        [SerializeField] InputReader input;
+        Rigidbody rb;
+
+        float brakeVelocity;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            input.Enable();
+
+            rb.centerOfMass = new Vector3(0f, -0.5f, 0f);
+
+            foreach (AxleInfo axleInfo in axleInfos)
+            {
+                axleInfo.originalForwardFriction = axleInfo.leftWheel.forwardFriction;
+                axleInfo.originalSidewaysFriction = axleInfo.leftWheel.sidewaysFriction;
+            }
+        }
+
+
+        void FixedUpdate()
+        {
+            float verticalInput = AdjustInput(input.Move.y);
+            float horizontalInput = AdjustInput(input.Move.x);
+
+            float motor = maxMotorTorque * verticalInput;
+            float steering = maxSteeringAngle * horizontalInput;
+
+            UpdateAxles(motor, steering);
+        }
+
+        void UpdateAxles(float motor, float steering)
+        {
+            foreach (AxleInfo axleInfo in axleInfos)
+            {
+                HandleSteering(axleInfo, steering);
+                HandleMotor(axleInfo, motor);
+                HandleBrakesAndDrift(axleInfo);
+                UpdateWheelVisuals(axleInfo.leftWheel);
+                UpdateWheelVisuals(axleInfo.rightWheel);
+            }
+        }
+
+
+        void UpdateWheelVisuals(WheelCollider collider)
+        {
+            if (collider.transform.childCount == 0) return;
+
+            Transform visualWheel = collider.transform.GetChild(0);
+
+            Vector3 position;
+            Quaternion rotation;
+            collider.GetWorldPose(out position, out rotation);
+
+            visualWheel.transform.position = position;
+            visualWheel.transform.rotation = rotation;
+        }
+
+
+        void HandleSteering(AxleInfo axleInfo, float steering)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+        }
+
+
+        void HandleMotor(AxleInfo axleInfo, float motor)
+        {
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+
+
+        void HandleBrakesAndDrift(AxleInfo axleInfo)
+        {
+            if (axleInfo.motor)
+            {
+                if (input.IsBraking)
+                {
+                    rb.constraints = RigidbodyConstraints.FreezeRotationX;
+
+                    float newZ = Mathf.SmoothDamp(rb.linearVelocity.z, 0, ref brakeVelocity, 1f);
+                    rb.linearVelocity = rb.linearVelocity.With(z: newZ);
+
+                    axleInfo.leftWheel.brakeTorque = brakeTorque;
+                    axleInfo.rightWheel.brakeTorque = brakeTorque;
+                }
+                else
+                {
+                    rb.constraints = RigidbodyConstraints.None;
+
+                    axleInfo.leftWheel.brakeTorque = 0;
+                    axleInfo.rightWheel.brakeTorque = 0;
+                }
+            }
+        }
+
+
+        float AdjustInput(float input)
+        {
+            return input switch
+            {
+                >= .7f => 1f,
+                <= -.7f => -1f,
+                _ => input
+            };
+        }
+    }
+    // Extension method to fix the "With" compilation error
+    public static class Vector3Extensions
+    {
+        public static Vector3 With(this Vector3 vector, float? x = null, float? y = null, float? z = null)
+        {
+            return new Vector3(x ?? vector.x, y ?? vector.y, z ?? vector.z);
+        }
+    }
+}
